6d23d3ed0a19afae43fc714dac0a94de8fb0bcc7
FrancoJulianRossi <rossi.francoj@gmail.com>
2025-10-30 11:02:27 -0300

    Room.controller creacion

---
+++ Backend/src/controllers/room.controller.ts
@@ -0,0 +1,11 @@
+import { rooms } from "../models/mocks/room.models";
+import { Room } from "../models/mocks/entities/room.entity";
+
+class RoomController {
+    public getAllRooms(): Room[] {
+        return rooms;
+    }
+    public getRoomById(id: string): Room | undefined {
+        return rooms.find(room => room.id === id);
+    }
+}
