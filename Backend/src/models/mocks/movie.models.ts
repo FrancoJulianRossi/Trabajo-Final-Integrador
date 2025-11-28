9cb8be6702bd087632a29fe42ba22fdf00a2ff13
Santiago Recari <santi.recari.sr@gmail.com>
2025-11-28 16:55:01 -0300

    Arreglo model movie

--- Backend/src/models/mocks/movie.models.ts
+++ Backend/src/models/mocks/movie.models.ts
@@ -1,124 +1,121 @@
-import { Movie } from "../mocks/entities/movie.entity";
+import { Movie } from "./entities/movie.entity";
 
-export const movies: Movie[] = [
-    new Movie(
-        1,
-        "Inception",
-        148,
-        "Un ladrón que roba secretos a través de los sueños debe realizar una última misión imposible.",
-        "Ciencia ficción",
-        "Estreno",
-        "Christopher Nolan",
-        "Inglés",
-        true,
-        "https://m.media-amazon.com/images/I/81p+xe8cbnL._AC_SL1500_.jpg"
-    ),
-    new Movie(
-        2,
-        "El Padrino",
-        175,
-        "La historia de una poderosa familia mafiosa italiana en Nueva York.",
-        "Drama",
-        "Clásico",
-        "Francis Ford Coppola",
-        "Inglés",
-        true,
-        "https://m.media-amazon.com/images/I/51rggtPgmRL._AC_.jpg"
-    ),
-    new Movie(
-        3,
-        "Titanic",
-        195,
-        "Un joven artista y una chica de alta sociedad se enamoran a bordo del Titanic.",
-        "Romance",
-        "Clásico",
-        "James Cameron",
-        "Inglés",
-        true,
-        "https://m.media-amazon.com/images/I/71yAzv7ZP-L._AC_SL1024_.jpg"
-    ),
-    new Movie(
-        4,
-        "Avatar",
-        162,
-        "Un exmarine se une a los habitantes de Pandora en una batalla por su supervivencia.",
-        "Ciencia ficción",
-        "Estreno",
-        "James Cameron",
-        "Inglés",
-        true,
-        "https://m.media-amazon.com/images/I/61OUGpUfAyL._AC_SL1024_.jpg"
-    ),
-    new Movie(
-        5,
-        "Avengers: Endgame",
-        181,
-        "Los Vengadores se reúnen para revertir el daño causado por Thanos.",
-        "Acción",
-        "Estreno",
-        "Anthony y Joe Russo",
-        "Inglés",
-        true,
-        "https://m.media-amazon.com/images/I/71niXI3lxlL._AC_SL1178_.jpg"
-    ),
-    new Movie(
-        6,
-        "The Dark Knight",
-        152,
-        "Batman enfrenta al Joker, un criminal que siembra el caos en Ciudad Gótica.",
-        "Acción",
-        "Clásico",
-        "Christopher Nolan",
-        "Inglés",
-        true,
-        "https://m.media-amazon.com/images/I/51T8cF7Tn-L._AC_.jpg"
-    ),
-    new Movie(
-        7,
-        "Forrest Gump",
-        142,
-        "Un hombre con una mente simple presencia los eventos más importantes de EE.UU.",
-        "Drama",
-        "Clásico",
-        "Robert Zemeckis",
-        "Inglés",
-        true,
-        "https://m.media-amazon.com/images/I/61r2L0-7qlL._AC_SL1024_.jpg"
-    ),
-    new Movie(
-        8,
-        "Jurassic Park",
-        127,
-        "Científicos clonan dinosaurios y crean un parque temático que pronto se descontrola.",
-        "Aventura",
-        "Clásico",
-        "Steven Spielberg",
-        "Inglés",
-        true,
-        "https://m.media-amazon.com/images/I/71e0v1v8SiL._AC_SL1000_.jpg"
-    ),
-    new Movie(
-        9,
-        "The Matrix",
-        136,
-        "Un hacker descubre la verdad sobre su realidad y su papel en la guerra contra las máquinas.",
-        "Ciencia ficción",
-        "Clásico",
-        "Lana y Lilly Wachowski",
-        "Inglés",
-        true,
-        "https://m.media-amazon.com/images/I/51EG732BV3L._AC_.jpg"
-    ),
-    new Movie(
-        10,
-        "Toy Story",
-        81,
-        "Los juguetes cobran vida cuando los humanos no están presentes.",
-        "Animación",
-        "Familiar",
-        "John Lasseter",
-        "Inglés",
-        true,
-        "https://m.media-amazon.com/images/I/71aG+xDKSYL._AC_SL1024_.jpg"
-    )
-]
+export class MoviesModel {
+    private data: Movie[];
+    private id: number;
+
+    constructor() {
+        this.data = [
+            new Movie(
+                1,
+                "Inception",
+                148,
+                "Sueños dentro de sueños.",
+                "Ciencia ficción",
+                "Estreno",
+                "Christopher Nolan",
+                "Inglés",
+                true,
+                "https://m.media-amazon.com/images/I/81p+xe8cbnL._AC_SL1500_.jpg"
+            ),
+        ];
+        this.id = 2;
+    }
+
+    seed(initial: Movie[]) {
+        this.data = initial.map(
+            (m, idx) =>
+                new Movie(
+                    m.idMovie || idx + 1,
+                    m.name,
+                    m.getDuration(),
+                    m.description,
+                    m.genre,
+                    m.categorie,
+                    m.director,
+                    m.lenguage,
+                    m.isSubtitled(),
+                    m.poster
+                )
+        );
+        this.id = this.data.length
+            ? Math.max(...this.data.map((x) => x.idMovie)) + 1
+            : 1;
+    }
+
+    list(): Promise<Movie[]> {
+        return Promise.resolve(this.data);
+    }
+
+    getById(id: number): Promise<Movie> {
+        return new Promise((resolve, reject) => {
+            const movie = this.data.find((m) => m.idMovie === id);
+            movie ? resolve(movie) : reject(new Error("Película no encontrada"));
+        });
+    }
+
+    create(movie: Movie): Promise<Movie> {
+        return new Promise((resolve, reject) => {
+            const exists = this.data.find(
+                (m) => m.name.toLowerCase() === movie.name.toLowerCase()
+            );
+            if (exists) {
+                reject(new Error("La película ya existe"));
+            } else {
+                const newMovie = new Movie(
+                    this.id++,
+                    movie.name,
+                    movie.getDuration(),
+                    movie.description,
+                    movie.genre,
+                    movie.categorie,
+                    movie.director,
+                    movie.lenguage,
+                    movie.isSubtitled(),
+                    movie.poster
+                );
+                this.data.push(newMovie);
+                resolve(newMovie);
+            }
+        });
+    }
+
+    update(id: number, updatedMovie: Movie): Promise<Movie> {
+        return new Promise((resolve, reject) => {
+            const index = this.data.findIndex((m) => m.idMovie === id);
+            if (index === -1) {
+                reject(new Error("Película no encontrada"));
+            } else {
+                const newMovie = new Movie(
+                    id,
+                    updatedMovie.name,
+                    updatedMovie.getDuration(),
+                    updatedMovie.description,
+                    updatedMovie.genre,
+                    updatedMovie.categorie,
+                    updatedMovie.director,
+                    updatedMovie.lenguage,
+                    updatedMovie.isSubtitled(),
+                    updatedMovie.poster
+                );
+                this.data[index] = newMovie;
+                resolve(newMovie);
+            }
+        });
+    }
+
+    delete(id: number): Promise<Movie> {
+        return new Promise((resolve, reject) => {
+            const movie = this.data.find((m) => m.idMovie === id);
+            if (!movie) {
+                reject(new Error("Película no encontrada"));
+                return;
+            }
+
+            this.data = this.data.filter((m) => m.idMovie !== id);
+            resolve(movie);
+        });
+    }
+}
+
+export default new MoviesModel();
