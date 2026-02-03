4707c4792e044bed405f2a0ec7e0da773c863c98
MartinAndresGarnica <mag.martin2000@gmail.com>
2026-02-03 11:28:32 -0300

    Reservation routes finalizado

--- Backend/src/routes/booking.routes.ts
+++ Backend/src/routes/booking.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getBookings, createBooking } from "../controllers/booking.controller";
+import { authenticate } from "../middleware/auth.middleware";
 
 const router = Router();
 
-router.get("/bookings", getBookings);
-router.post("/bookings", createBooking);
+router.get("/bookings", authenticate, getBookings);
+router.post("/bookings", authenticate, createBooking);
 
 export default router;
