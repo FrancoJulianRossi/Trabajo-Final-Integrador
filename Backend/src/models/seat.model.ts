d4e180a40c88c4b313bbb995f4c5d1901964bb49
FrancoJulianRossi <rossi.francoj@gmail.com>
2026-03-01 12:20:46 -0300

    actualizacion seat backend

---
+++ Backend/src/models/seat.model.ts
@@ -0,0 +1,41 @@
+import { Table, Column, Model, DataType, ForeignKey, BelongsTo } from "sequelize-typescript";
+import { Room } from "./room.model";
+
+@Table({
+  tableName: "seats",
+  timestamps: false,
+})
+export class Seat extends Model {
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  declare idSeat: number;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  declare row: number;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  declare column: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    defaultValue: "Standard",
+  })
+  declare type: string;
+
+  @ForeignKey(() => Room)
+  @Column
+  declare roomId: number;
+
+  @BelongsTo(() => Room)
+  declare room: Room;
+}
